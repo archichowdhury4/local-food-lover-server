c89c025b58eab6869960a964b794b39d79f74a56
archichowdhury4 <archicmpi@gmail.com>
2025-11-10 10:08:45 +0600

    added insert & delete operation in reviewCollection

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const cors = require("cors")
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express()
 const port = process.env.PORT || 3000;
 
@@ -10,10 +11,53 @@ app.use(express.json());
 
 
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+// password
+// ypl0Iu2wxL5vQao8
+const uri = "mongodb+srv://localdbUser:ypl0Iu2wxL5vQao8@cluster0.vvoht34.mongodb.net/?appName=Cluster0";
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+app.get("/", (req, res) =>{
+  res.send("Smart server is running")
 })
 
+async function run() {
+  try {
+    
+    await client.connect();
+
+    const db = client.db("review_db")
+    const reviewCollection = db.collection("reviews")
+
+
+     app.post("/reviews", async(req,res) =>{
+        const newReviews =req.body;
+        const result = await reviewCollection.insertOne(newReviews);
+        res.send(result);
+    })
+
+     app.delete("/reviews/:id", async(req,res) =>{
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)}
+      const result = await reviewCollection.deleteOne(query)
+      res.send(result)
+    })
+     await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  }
+    finally{
+
+
+    }
+}
+run().catch(console.dir);
+
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`local food lover server is running on port ${port}`)
 })
