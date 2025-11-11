d91aa241ea8c6d64d4826627d41c222cc5c17e5f
archichowdhury4 <archicmpi@gmail.com>
2025-11-11 11:28:20 +0600

    added userscollection for register

--- index.js
+++ index.js
@@ -34,20 +34,60 @@ async function run() {
 
     const db = client.db("review_db")
     const reviewCollection = db.collection("reviews")
-
-
+    const usersCollection = db.collection("users")
+    // reviewCollection 
+     app.get("/reviews", async(req,res) =>{
+      const cursor = reviewCollection.find();
+        const result = await cursor.toArray();
+        res.send(result);
+    })
+// Insert
      app.post("/reviews", async(req,res) =>{
         const newReviews =req.body;
         const result = await reviewCollection.insertOne(newReviews);
         res.send(result);
     })
+// Update
+     app.patch("/reviews/:id", async(req, res) =>{
+      const id = req.params.id;
+       const updateReviews =req.body;
+      const query = {_id: new ObjectId(id)}
+      const update ={
+        $set:{
+          name: updateReviews.name,
+          price: updateReviews.price
+        }
+      }
+       const result = await reviewCollection.updateOne(query, update);
+        res.send(result);
+    })
 
+// delete
      app.delete("/reviews/:id", async(req,res) =>{
       const id = req.params.id;
       const query = {_id: new ObjectId(id)}
       const result = await reviewCollection.deleteOne(query)
       res.send(result)
     })
+
+
+    // usersCollection
+
+     app.post('/users', async (req, res) => {
+           const newUser = req.body;
+            const email = req.body.email;
+            const query = { email: email }
+            const existingUser = await usersCollection.findOne(query);
+
+            if (existingUser) {
+                res.send({ message: 'user already exits. do not need to insert again' })
+            }
+            else {
+                const result = await usersCollection.insertOne(newUser);
+                res.send(result);
+            }
+        })
+
      await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   }
