b7fdbbb47381f6f16957938bc19f8ea6943e3b1c
archichowdhury4 <archicmpi@gmail.com>
2025-11-13 23:13:11 +0600

    added my finishing work

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ app.get("/", (req, res) =>{
 async function run() {
   try {
     
-    await client.connect();
+    // await client.connect();
 
     const db = client.db("review_db")
     const reviewCollection = db.collection("reviews")
@@ -190,7 +190,7 @@ app.delete("/favorites/:id", async (req, res) => {
 
 
 
-     await client.db("admin").command({ ping: 1 });
+    //  await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   }
     finally{
